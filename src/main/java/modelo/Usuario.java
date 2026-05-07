d74afaa855241b8088f26f5017f38e747d70dd92
sebastiancolomera <s.colomera01@ufromail.cl>
2026-05-07 02:02:03 -0400

    feat: Implementar modelo Usuario

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -1,4 +1,63 @@
 package modelo;
 
+import java.time.LocalDate;
+
 public class Usuario {
+    private String nombre;
+    private String contrasena;
+    private String rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String contrasena, String rol) {
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public boolean esSuperusuario() {
+        if (rol.equals("superusuario")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean esLider() {
+        if (rol.equals("lider")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean esUsuario() {
+        if (rol.equals("usuario")) {
+            return true;
+        }
+        return false;
+    }
 }
