887841ce2e9c1fcf7db0b543813731ddd3f0a9d2
sebastiancolomera <s.colomera01@ufromail.cl>
2026-05-07 01:54:58 -0400

    feat: Implementar controlador de inicio Main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,17 @@
 package app;
 
+import vista.VistaCLI;
+
 public class Main {
+    public static void main(String[] args) {
+        // 1. Creamos el cerebro (Controlador)
+        GestorSigma controlador = new GestorSigma();
+
+        // 2. Creamos un usuario inicial para poder entrar (ya que no hay persistencia aún)
+        controlador.registrarUsuario("admin", "admin123", "superusuario");
+
+        // 3. Iniciamos la interfaz pasándole el controlador
+        VistaCLI vista = new VistaCLI(controlador);
+        vista.iniciar();
+    }
 }
