6de928005f88f09a884f46f6b7b13b6bc444d9e9
sduran03-git <s.duran03@ufromail.cl>
2026-05-07 00:49:48 -0400

    feat: imports

--- src/main/java/persistencia/GestorJSON.java
+++ src/main/java/persistencia/GestorJSON.java
@@ -1,4 +1,18 @@
 package persistencia;
 
+import modelo.Meta;
+import modelo.Usuario;
+import modelo.Tarea;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class GestorJSON {
 }
