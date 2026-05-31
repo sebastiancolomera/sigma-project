3e7cef57c93375e076164b034c03d5e426c8ac66
sduran03-git <s.duran03@ufromail.cl>
2026-05-31 19:10:41 -0400

    refactor: actualizar Main para usar la persistencia

--- src/main/java/sigma/app/Main.java
+++ src/main/java/sigma/app/Main.java
@@ -5,18 +5,20 @@ import sigma.vista.gui.LoginFrame;
 import javax.swing.SwingUtilities;
 
 public class Main {
+
     public static void main(String[] args) {
+
         GestorSigma controlador = new GestorSigma();
 
         controlador.cargarDatos();
 
-        if (comtrolador.getUsuario().isEmpty()) {
+        if (controlador.getUsuarios().isEmpty()) {
             controlador.registrarUsuario("admin", "admin123", RolUsuario.SUPERUSUARIO);
         }
 
-        SwingUtilities.invokeLater(()-> {
-            LoginFrame loginFrame = new LoginFrame(controlador);
-            loginFrame.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            LoginFrame login = new LoginFrame(controlador);
+            login.setVisible(true);
         });
     }
 }
